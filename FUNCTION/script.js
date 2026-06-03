2833f75b00401c98582c7336a2fc8b1e34f2e64a
Saba Zubair <saba214214@gmail.com>
2026-06-03 09:20:56 +0500

    functions

---
+++ FUNCTION/script.js
@@ -0,0 +1,45 @@
+// function
+function MyName() {
+  console.log("My name is Saba ");
+  console.log("My name is Saba ");
+  console.log("My name is Saba ");
+  console.log("My name is Saba ");
+  console.log("My name is Saba ");
+  console.log("My name is Saba ");
+  console.log("My name is Saba ");
+  console.log("My name is Saba ");
+  console.log("My name is Saba ");
+  console.log("My name is Saba ");
+}
+// calling a function
+MyName();
+
+// addition in function
+function addition(a,b){
+  let answer= a+b
+  console.log(`Answer of adding two num is ${answer}`);  
+}
+addition(1,2)
+addition(17,21)
+
+// subtraction in function
+function subtraction(p,q){
+  let answer= p-q 
+  console.log(`answer of subtracting two num is ${answer}`);
+}
+subtraction(8,4)
+subtraction(22,12)
+// multiplication in funtion
+function multiplication(r,s) {
+  let answer= r*s
+  console.log(`anwer of multiplying two num is ${answer}`);
+}
+multiplication(9,3)
+multiplication(4,5)
+// division in function
+function division(c,d){
+let answer= d/c
+console.log(`answer of dividing two num is ${answer}`);
+}
+division(2,22)
+division(3,36)
