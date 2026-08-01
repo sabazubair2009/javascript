f138c9f3dba58ce7cf23119fc6748ef11d01d986
Saba Zubair <saba214214@gmail.com>
2026-08-01 15:06:08 +0500

    types of loops

--- loops/script.js
+++ loops/script.js
@@ -1,4 +1,4 @@
-// condition 1
+// ==FOR LOOP==
 for (let a = 1; a < 10; a++) {
   console.log("Saba");
 }
@@ -6,10 +6,28 @@ for (let a = 1; a < 10; a++) {
 for (let a = 1; a < 10; a++) {
   console.count("Saba");
 }
-// Calculating the sum 
+// Calculating the sum
 let num = 0;
 for (let i = 1; i <= 5; i++) {
-  num = num + i;  
-//the loop is working like this 0+1+2+3+4+5= 15
+  num = num + i;
+  //the loop is working like this 0+1+2+3+4+5= 15
 }
 console.log("num =", num);
+
+// ==WHILE LOOP==
+// initialization
+let i = 1;
+// condition
+while (i <= 6) {
+  console.log("i=", i);
+  // updation
+  i++;
+}
+
+// ==DO_WHILE LOOP==
+let i=15;
+do{
+  console.log("loops");
+  i++;
+}while (i<=8);
+// Do-while loop runs atleast one time even if the condition is not true.
