183b603169e1b1d44c1a630ad24d00b102a959ea
Saba Zubair <saba214214@gmail.com>
2026-08-01 06:42:35 +0500

    loops

---
+++ loops/script.js
@@ -0,0 +1,12 @@
+// condition 1
+for (let a = 1; a < 10; a++) {
+  console.log("Saba");
+}
+// console.count is used to count how many times a particular function has been executed
+for (let a = 1; a < 10; a++) {
+  console.count("Saba");
+}
+// condition 2
+for (let a = 1; a < 10; a= a + 4) {
+  console.log(a);
+}
